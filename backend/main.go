ecd2b57e6d21632c371aaaee6e3b86ab64b84913
LobanMihajlo <loban.mihajlo2411@gmail.com>
2025-06-18 22:33:06 +0300

    feat: implement getUsers, createUser and updateUser funcs

--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
-	// "github.com/gorilla/mux"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 type User struct {
@@ -13,24 +17,116 @@ type User struct {
 	Email string `json:"email"`
 }
 
+func getUsers(db *sql.DB) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		rows, err := db.Query("SELECT id, name, email FROM users")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		users := []User{}
+		for rows.Next() {
+			var user User 
+			if err := rows.Scan(&user.Id, &user.Name, &user.Email); err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, user)
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(res).Encode(users)
+	}
+}
+
+func createUser(db *sql.DB) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		var user User 
+		if err := json.NewDecoder(req.Body).Decode(&user); err != nil {
+			log.Fatal(err)
+		}
+
+		err := db.QueryRow("INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id", 
+		user.Name, user.Email).Scan(&user.Id)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(res).Encode(user)
+	}
+}
+
+func updateUser(db *sql.DB) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		var user User 
+		if err := json.NewDecoder(req.Body).Decode(&user); err != nil {
+			log.Fatal(err)
+		}
+
+		vars := mux.Vars(req)
+		id := vars["id"]
+
+		_, err := db.Exec("UPDATE users SET name = $1, email = $2 WHERE id = $3", user.Name, user.Email, id)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var updatedUser User
+		err = db.QueryRow("SELECT id, name, email FROM users WHERE id = $1", id).Scan(&updatedUser.Id, &updatedUser.Name, &updatedUser.Email)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(res).Encode(&updatedUser)
+	}
+}
+
 func main() {
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-
+	
 	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT, email TEXT)")
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	
 	// now err, because none of this func exists yet
-	// router := mux.NewRouter()
-	// router.HandleFunc("/api/go/users", getUsers(db)).Methods("GET")
-	// router.HandleFunc("/api/go/users", createUsers(db)).Methods("POST")
+	router := mux.NewRouter()
+	router.HandleFunc("/api/go/users", getUsers(db)).Methods("GET")
+	router.HandleFunc("/api/go/users", createUser(db)).Methods("POST")
 	// router.HandleFunc("/api/go/users/{id}", getUser(db)).Methods("GET")
-	// router.HandleFunc("/api/go/users/{id}", updateUser(db)).Methods("PUT")
+	router.HandleFunc("/api/go/users/{id}", updateUser(db)).Methods("PUT")
 	// router.HandleFunc("/api/go/users/{id}", deleteUser(db)).Methods("DELETE")
+	
+	enhancedRouter := enableCORS(jsonContentTypeMiddleware(router))
+	
+	log.Fatal(http.ListenAndServe(":8000", enhancedRouter))
+}
 
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Access-Control-Allow-Origin", "*")
+		res.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		res.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if req.Method == "OPTIONS" {
+			res.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(res, req)
+	})
+}
+
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(res, req)
+	})
 }
